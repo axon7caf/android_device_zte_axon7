21b7651c39324843eafa552312fcd05db1787e6e
Dante Russo <drusso@codeaurora.org>
2017-02-03 18:21:08 -0800

    axon7: Add build rule for new location modules
    libgnss and liblocation_api added for LocationAPI
    implementation. android.hardware.gnss@1.0-impl-qti
    added for hidl gnss hal
    
    Change-Id: If1c99ed6597b267cd57b2cffd02e6c00275d60b7
    CRs-fixed: 1112712

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -4,6 +4,9 @@ PRODUCT_PACKAGES += \
     libcurl \
     libgnsspps
 
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl-qti
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
