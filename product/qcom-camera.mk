a6eb38611ab159ef6dd1f6925c4ed2b2f4b35f78
Bharat Pawar <bpawar@codeaurora.org>
2017-04-10 20:43:12 +0530

    Camera: Add HAL1
    Add Camera HAL1 implemetation
    
    Change-Id: Icde8d76bdf4ad2fb03f47c7674bad485d6aadb73

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -1,6 +1,7 @@
 # Camera
 PRODUCT_PACKAGES += \
     Snap \
+    camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
