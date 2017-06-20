d1955110366c0d7bde83a452fbfd90d7ccd5ca30
Baldev Sahu <bsahu@codeaurora.org>
2017-06-20 16:40:24 +0530

    axon7: Set debug.sf.enable_hwc_vds property to true
    Set above property to enable wfd use hwc.
    
    Change-Id: Id6969228760f69469281b48b0267f25d3c1d5880

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -26,6 +26,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=1 \
     debug.gralloc.enable_fb_ubwc=1 \
+    debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
     dev.pm.dyn_samplingrate=1 \
     persist.demo.hdmirotationlock=false \
