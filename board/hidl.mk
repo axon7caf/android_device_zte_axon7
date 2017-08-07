e7b7afb8b5e21100dcf8681b26e1cdb4e3a947e7
Mahantesh Sanakall <msanak@codeaurora.org>
2017-08-07 16:42:06 +0530

    axon7: Use build variables to build device matrices.
    Move away from building device manifest through PRODUCT_COPY_FILES.
    Use the build variables - DEVICE_MANIFEST_FILE and DEVICE_MATRIX_FILE
    to build device manifest and device compatibility files repectively.
    
    Change-Id: Ieb3e410118ee9fe00c6b03e32a983e584960d8dc

---
+++ board/hidl.mk
@@ -0,0 +1,3 @@
+# Hidl
+DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/configs/manifest.xml
+DEVICE_MATRIX_FILE   := $(LOCAL_PATH)/configs/compatibility_matrix.xml
--- product/hidl.mk
+++
@@ -1,3 +0,0 @@
-# HIDL
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/manifest.xml:$(TARGET_COPY_OUT_VENDOR)/manifest.xml
