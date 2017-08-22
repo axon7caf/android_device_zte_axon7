757893f4f4a4f812b0af73b0e80a6de09b328104
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 13:38:19 -0700

    axon7: Add RenderScript HAL
    Change-Id: Ic33704c7eb7e5977e6cbfe4252e436f4dc892fa5

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -7,14 +7,15 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     memtrack.msm8996 \
     qdcm_calib_data_zteSAM_S6E3HA3_SAM_25601440_5P5Inch.xml \
+    android.hardware.configstore@1.0-service \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    android.hardware.configstore@1.0-service
+    android.hardware.renderscript@1.0-impl
 
 # Permissions
 PRODUCT_COPY_FILES += \
