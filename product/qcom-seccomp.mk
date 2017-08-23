d36e972cbdf222a45a011489868e1b8564157aaa
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-23 08:35:15 -0700

    axon7: Add caf mediaserver seccomp policies
    Change-Id: I70ab0cd61e8bf8bceb9553534157e03178ea89af

---
+++ product/qcom-seccomp.mk
@@ -0,0 +1,4 @@
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
