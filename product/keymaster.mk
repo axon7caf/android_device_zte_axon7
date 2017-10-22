8fad4c0a3448a802f921b895e27b0f6da5dfed79
c457 <android.c357@gmail.com>
2017-10-22 06:37:01 -0500

    axon7: Build keymaster HAls
    Change-Id: I166a891d64cbfc70e818010053be0217fdeaa06f

--- product/keymaster.mk
+++ product/keymaster.mk
@@ -1,3 +1,6 @@
 # Keymaster Impl HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
